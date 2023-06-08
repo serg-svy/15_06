64668b158a832aedd6e7f82ae0b499499e6809d0
serg-svy <97796536+serg-svy@users.noreply.github.com>
2023-06-08 07:53:54 +0300

    up

--- ex_01.py
+++ ex_01.py
@@ -2,3 +2,5 @@ fh = open('test.txt', 'w')
 symbols_written = fh.write('hello!')
 print(symbols_written)  # 6
 fh.close()
+
+# В этом примере мы открыли файл test.txt для записи и записали туда строку 'hello!' длиной в 6 символов.
